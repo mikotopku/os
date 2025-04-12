8e393b184b417ab5c113d9a8bdebbf28a7eae745
zzj <2300011474@stu.pku.edu.cn>
2025-04-13 00:08:16 +0800

    ch1-function

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,20 @@ use core::arch::global_asm;
 
 mod lang_items;
 
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
 global_asm!(include_str!("entry.asm"));

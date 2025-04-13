ac49edb748aa6651042e2d68134eba529810f463
zzj <2300011474@stu.pku.edu.cn>
2025-04-13 15:14:16 +0800

    ch1-final-3

--- src/console.rs
+++ src/console.rs
@@ -33,7 +33,7 @@ macro_rules! println {
 #[macro_export]
 macro_rules! error {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!("\x1b[31m", $fmt, "\x1b[0m") $(, $($arg)+)?));
+        $crate::console::print(format_args!(concat!("\x1b[31m[ERROR] ", $fmt, "\x1b[0m") $(, $($arg)+)?));
     };
 }
 
@@ -41,7 +41,7 @@ macro_rules! error {
 #[cfg(all(any(LOG_WARN, LOG_INFO, LOG_DEBUG, LOG_TRACE), not(LOG_ERROR)))]
 macro_rules! warn {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!("\x1b[93m", $fmt, "\x1b[0m") $(, $($arg)+)?));
+        $crate::console::print(format_args!(concat!("\x1b[93m[WARN] ", $fmt, "\x1b[0m") $(, $($arg)+)?));
     };
 }
 #[cfg(all(not(any(LOG_WARN, LOG_INFO, LOG_DEBUG, LOG_TRACE)), LOG_ERROR))]
@@ -53,7 +53,7 @@ macro_rules! warn {
 #[cfg(all(any(LOG_INFO, LOG_DEBUG, LOG_TRACE), not(any(LOG_WARN, LOG_ERROR))))]
 macro_rules! info {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!("\x1b[34m", $fmt, "\x1b[0m") $(, $($arg)+)?));
+        $crate::console::print(format_args!(concat!("\x1b[34m[INFO] ", $fmt, "\x1b[0m") $(, $($arg)+)?));
     };
 }
 #[cfg(all(not(any(LOG_INFO, LOG_DEBUG, LOG_TRACE)), any(LOG_WARN, LOG_ERROR)))]
@@ -66,7 +66,7 @@ macro_rules! info {
 #[cfg(all(any(LOG_DEBUG, LOG_TRACE), not(any(LOG_INFO, LOG_WARN, LOG_ERROR))))]
 macro_rules! debug {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!("\x1b[32m", $fmt, "\x1b[0m") $(, $($arg)+)?));
+        $crate::console::print(format_args!(concat!("\x1b[32m[DEBUG] ", $fmt, "\x1b[0m") $(, $($arg)+)?));
     };
 }
 #[cfg(all(not(any(LOG_DEBUG, LOG_TRACE)), any(LOG_INFO, LOG_WARN, LOG_ERROR)))]
@@ -78,7 +78,7 @@ macro_rules! debug {
 #[cfg(all(any(LOG_TRACE), not(any(LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR))))]
 macro_rules! trace {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!("\x1b[90m", $fmt, "\x1b[0m") $(, $($arg)+)?));
+        $crate::console::print(format_args!(concat!("\x1b[90m[TRACE] ", $fmt, "\x1b[0m") $(, $($arg)+)?));
     };
 }
 #[cfg(all(not(any(LOG_TRACE)), any(LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR)))]

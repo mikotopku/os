8d741859cd22737e14b18c03e453365fdb2182a0
zzj <2300011474@stu.pku.edu.cn>
2025-04-18 10:01:47 +0800

    ch3-coop-final

--- src/syscall/process.rs
+++ src/syscall/process.rs
@@ -8,3 +8,10 @@ pub fn sys_exit(exit_code: i32) -> ! {
     exit_current_and_run_next();
     panic!("Unreachable in sys_exit");
 }
+
+use crate::task::suspend_current_and_run_next;
+
+pub fn sys_yield() -> isize {
+    suspend_current_and_run_next();
+    0
+}
